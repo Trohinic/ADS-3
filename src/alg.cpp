e213ddd2c1d539d0c02c47a7e913803955b044bf
Trohinic <159806490+Trohinic@users.noreply.github.com>
2024-04-02 17:28:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,51 +1,44 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int* arr, int size, int value) {
-	int cur1;
-	int start = 0;
-	int end = size - 1;
+  int cur1;
+  int start = 0;
+  int end = size - 1;
 
-	while (end - start > 1) {
-		cur1 = start + (end-start) / 2;
-		if (arr[cur1] < value) {
-			start = cur1;
-		}
-		else { 
-			end = cur1; 
-		}
-		
-	}
+  while (end - start > 1) {
+    cur1 = start + (end - start) / 2;
+    if (arr[cur1] < value) {
+      start = cur1;
+    } else {
+      end = cur1;
+    }
+  }
 
-	if (arr[start] == value) {
-		cur1 = start;
-	}
-	else if (arr[end] == value) {
-		cur1 = end;
-	}
-	else {
-		return 0;
-	}
+  if (arr[start] == value) {
+    cur1 = start;
+  } else if (arr[end] == value) {
+    cur1 = end;
+  } else {
+    return 0;
+  }
 
-	end = size - 1;
-	int cur2;
+  end = size - 1;
+  int cur2;
 
-	while (end - start > 1) {
-		cur2 = start + (end - start) / 2;
-		if (arr[cur2] <= value) {
-			start = cur2;
-		}
-		else {
-			end = cur2;
-		}
+  while (end - start > 1) {
+    cur2 = start + (end - start) / 2;
+    if (arr[cur2] <= value) {
+      start = cur2;
+    } else {
+      end = cur2;
+    }
+  }
 
-	}
-
-	if (arr[start] == value) {
-		cur2 = start;
-	}
-	else if (arr[end] == value) {
-		cur2 = end;
-	}
-
-	return cur2 - cur1 + 1;
+  if (arr[end] == value) {
+    cur2 = end;
+  } else if (arr[start] == value) {
+    cur2 = start;
+  }
+  return cur2 - cur1 + 1;
 }
+
